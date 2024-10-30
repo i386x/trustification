172e79ba1b7cd76f2fb7229a0a984f723190fe9c
Jiri Kucera <jkucera@redhat.com>
2024-10-30 13:54:44 +0100

    WIP

--- integration-tests/tests/spog.rs
+++ integration-tests/tests/spog.rs
@@ -45,7 +45,7 @@ async fn spog_endpoints(context: &mut SpogContext) {
 }
 
 /// SPoG is the entrypoint for the frontend. It exposes a search API, but forwards requests
-/// to bombastic of vexination. This requires forwarding the token too. This test is here to
+/// to bombastic or vexination. This requires forwarding the token too. This test is here to
 /// test this.
 #[test_context(SpogContext)]
 #[tokio::test]
@@ -65,7 +65,7 @@ async fn spog_search_forward_bombastic(context: &mut SpogContext) {
 }
 
 /// SPoG is the entrypoint for the frontend. It exposes a search API, but forwards requests
-/// to bombastic of vexination. This requires forwarding the token too. This test is here to
+/// to bombastic or vexination. This requires forwarding the token too. This test is here to
 /// test this.
 #[test_context(SpogContext)]
 #[tokio::test]
